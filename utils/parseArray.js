4a1b9ec277fc95de7aaf5104d6da9c50c1b23e9f
GammaMicrowave <pranavt150@gmail.com>
2023-11-04 19:02:07 +0530

    added utility functions to parse string and object

---
+++ utils/parseArray.js
@@ -0,0 +1,25 @@
+export function arrayToString(data) {
+  const parts = [];
+
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const values = data[key].join(";");
+      parts.push(`${key}:${values}`);
+    }
+  }
+
+  return parts.join("-");
+}
+
+export function stringToObject(string) {
+  const parts = string.split("-");
+  const result = {};
+
+  for (const part of parts) {
+    const [key, values] = part.split(":");
+    const valueArray = values.split(";");
+    result[key] = valueArray;
+  }
+
+  return result;
+}
