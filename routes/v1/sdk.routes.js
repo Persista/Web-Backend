d53bbc0bb345761a235335d32b20e022fcfa7bf6
Anurag Jain <anurag10jain@gmail.com>
2023-11-05 03:12:45 +0530

    Added sdk routes

--- routes/v1/sdk.routes.js
+++ routes/v1/sdk.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
+
+import { getLLMResponse, createChat } from "../../controllers/sdk/chat.controller";
+import { verifyApiKey } from "../../middleware/sdk.middleware";
+
 var router = Router();
 
-// router.post("/", getLLMResponse);
+router.post("/chat/getresponse", verifyApiKey, getLLMResponse);
+router.post("/chat/create", verifyApiKey, createChat);
+
 export default router;
