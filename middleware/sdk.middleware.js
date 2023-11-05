04e13c17cdf17575e747a2911fd6c1c990470c28
Anurag Jain <anurag10jain@gmail.com>
2023-11-05 09:34:04 +0530

    Changed sdk middleware

--- middleware/sdk.middleware.js
+++ middleware/sdk.middleware.js
@@ -2,17 +2,17 @@ import { response_401, response_500 } from "../utils/responseCodes.js";
 import prisma from "../config/db.config.js";
 
 export async function verifyApiKey(req, res, next) {
-    const key = req.header("Authorization");
-    if (!key) return response_401(res, "Unauthorized");
-    try {
-        const match = await prisma.project.findUnique({
-					where: {
-						primaryApiKey : key,
-					},
-				});
-        if (!match) return response_401(res, "Unauthorized");
-        next();
-    } catch (err) {
-        return response_500(res, err);
-    }
+  const key = req.header("Authorization");
+  if (!key) return response_401(res, "Unauthorized");
+  try {
+    const match = await prisma.project.findFirst({
+      where: {
+        primaryApiKey: key,
+      },
+    });
+    if (!match) return response_401(res, "Unauthorized");
+    next();
+  } catch (err) {
+    return response_500(res, err);
+  }
 }
